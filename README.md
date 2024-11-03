4dde3b3ebcfdc59729c99828ee43970a1c8a82f9
mahmouddeshoo <mahmouddshoo@gmail.com>
2024-11-04 00:43:03 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Js-First-Task
---
+++ js/main.js
@@ -0,0 +1,38 @@
+// let code the first part 
+let num1= 10;
+let num2= 3;
+
+ let sum = num1 + num2;         // addition
+let difference = num1 - num2;   //Subtraction
+let product = num1 * num2;      //Multiplication
+let quotient = num1 / num2;     //Division
+let remainder = num1 % num2;    //Modulus
+
+
+console.log("مجموع الأعداد :", sum);
+console.log(" الأعداد فرق:", difference);
+console.log("الضرب حاصل:", product);
+console.log("القسمة ناتج:", quotient);
+console.log("القسمة باقي:", remainder);
+
+
+
+// let code the second part
+let isGreater = num1 > num2;
+let isLess = num1 < num2;
+let isEqual = num1 === num2;
+let isNotEqual = num1 !== num2;
+
+
+console.log("هل num1 من أكبر num2؟", isGreater);
+console.log("هل num1 من أصغر num2؟", isLess);
+console.log("هل num1 يساوي num2؟", isEqual);
+console.log("هل num1 يساوي ال num2؟", isNotEqual);
+
+
+
+// let code the third part
+let firstName = "Mohammed";
+let lastName = "Al Ahmed";
+let fullName = firstName + " " + lastName;
+console.log("Your Name: ", fullName);
